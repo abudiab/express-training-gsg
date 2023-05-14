f3393c18616d134e8becc9fa40761213925f7fe2
Mahmoud Diab <mahmoud.w.diab@gmail.com>
2023-05-14 12:57:06 +0300

    implemented the middleware to validate todos and check if the id exists

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { json } from "express";
-import { todos } from "./mock_data.js";
+import { todos as initialTodos } from "./mock_data.js";
+
+let todos = initialTodos;
 
 const app = express();
 const PORT = 3001;
@@ -14,17 +16,29 @@ app.get("/", (req, res) => {
 
 app.get("/todos", (req, res) => res.status(200).send(todos));
 
-app.get("/todos/:id", (req, res) => {
+//Middleware to check if todo with specific id exists (implemented the better way)
+
+const checkIfIDExists = (req, res, next) => {
   const id = JSON.parse(req.params.id);
   const todo = todos.find((todo) => todo.id == id);
-  if (todo) {
-    res.status(200).send(todo);
-  } else {
+  if (!todo) {
     res.status(404).send("Can't find the specified todo");
   }
+  next();
+};
+
+app.get("/todos/:id", checkIfIDExists, (req, res) => {
+  const id = JSON.parse(req.params.id);
+  const todo = todos.find((todo) => todo.id == id);
+  //   if (todo) {
+  //     res.status(200).send(todo);
+  //   } else {
+  //     res.status(404).send("Can't find the specified todo");
+  //   }
+  res.status(200).send(todo);
 });
 
-app.delete("/todos/:id", (req, res) => {
+app.delete("/todos/:id", checkIfIDExists, (req, res) => {
   if (todos.length == 0) {
     res.status(422).send("No todos left");
   }
@@ -39,9 +53,9 @@ app.delete("/todos/:id", (req, res) => {
   res.status(200).send(response);
 });
 
-//another way to handle queries instead of params
+//another way to handle queries instead of params (redundant for now)
 
-app.delete("/todos", (req, res) => {
+app.delete("/todos", checkIfIDExists, (req, res) => {
   if (todos.length == 0) {
     res.status(422).send("No todos left");
   }
@@ -53,9 +67,22 @@ app.delete("/todos", (req, res) => {
   res.status(204).send(response);
 });
 
+//middleware to check the todo from the post request (implementing this using the new method on the documentation)
+const validateTask = (req, res, next) => {
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    return res.status(400).send("Title and description are required");
+  }
+
+  next();
+};
+
+app.use(validateTask);
+
 //create a todo
 app.post("/todos", (req, res) => {
-  console.log("this is the body", req.body);
+  //   console.log("this is the body", req.body);
 
   const { title, description } = req.body;
   const id = todos.length + 1; //this is not the best thing, but just for the sake of it.
@@ -63,24 +90,26 @@ app.post("/todos", (req, res) => {
   todos.push(newTodo);
   res.send(`Created new todo with ID ${id}`);
 });
+//end of middleware implementation the documentation method
 
 //update a todo
-app.put("/todos/:id", (req, res) => {
+app.put("/todos/:id", checkIfIDExists, (req, res) => {
   const id = JSON.parse(req.params.id);
   const { title, description, completionStatus } = req.body;
   const todo = todos.find((todo) => todo.id === id);
-  if (todo) {
-    todo.title = title || todo.title;
-    todo.completionStatus = completionStatus || todo.completionStatus;
-    todo.description = description || todo.description;
-    res.send(`Updated todo with ID ${id}`);
-  } else {
-    res.status(404).send("Todo not found");
-  }
+  //   if (todo) {
+  todo.title = title || todo.title;
+  todo.completionStatus = completionStatus || todo.completionStatus;
+  todo.description = description || todo.description;
+  res.send(`Updated todo with ID ${id}`);
+  //   } else {
+  //     res.status(404).send("Todo not found");
+  //   }
 });
 
 const todoDeleteHandler = (id) => {
-  return todos.filter((todo) => todo.id != id);
+  todos = todos.filter((todo) => todo.id != id);
+  return todos;
 };
 
 app.listen(PORT, () => {
